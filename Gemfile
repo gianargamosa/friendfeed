5d6a5193e461b5b67327b79811f0da896a37e043
Arman Gian <armanargamosa@e-lawsolutions.com>
2016-09-30 15:07:20 +0800

    task1-done image upload for avatar

--- Gemfile
+++ Gemfile
@@ -16,6 +16,11 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'pry'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :friendships
   has_many :friends, :through => :friendships
+
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 
   def add_friend(friend)
     friendship = friendships.build(:friend_id => friend.id)
@@ -25,4 +29,9 @@ class User < ApplicationRecord
     followers_ids = Friendship.where("friend_id = :mine",{:mine => self.id}).map { |f|  f.user_id  }
     User.where("id in(:followers_ids)", {:followers_ids => followers_ids})
   end
+
+  private
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+    end
 end
